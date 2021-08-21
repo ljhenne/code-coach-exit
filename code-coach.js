ab322671bf4f1a6d355edcbb672427c0c108905d
Logan Hennessy <loganjhennessy@gmail.com>
2021-08-21 12:00:22 -0700

    Get question title from page;

--- code-coach.js
+++ code-coach.js
@@ -1,1 +1,12 @@
-document.body.style.border = "5px solid red";
+console.log("Code coach")
+
+function waitForQuestionTitle() {
+    let questionTitleElements = document.querySelectorAll("[data-cy='question-title']");
+    if (questionTitleElements.length < 1) {
+        window.requestAnimationFrame(waitForQuestionTitle);
+    } else {
+        console.log(questionTitleElements[0].textContent);
+    }
+}
+
+waitForQuestionTitle();
